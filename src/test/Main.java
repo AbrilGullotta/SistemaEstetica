9f13f5566f89e195e4b9f256f6093ce5870e8ad6
AbrilGullotta <abril.gullotta@davinci.edu.ar>
2026-05-18 10:49:32 -0300

    agregar encriptador

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,24 +3,32 @@ package test;
 import modelo.Cliente;
 import repository.ClienteRepository;
 
-public class Main {
+import util.Encriptador;public class Main {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+//
+//		Cliente cliente = new Cliente();
+//
+//		cliente.setNombre("Lucia");
+//		cliente.setApellido("Fernandez");
+//		cliente.setDni(87654321);
+//		cliente.setEmail("lucia@gmail.com");
+//		cliente.setTelefono("1199988877");
+//		cliente.setUsuario("luciaf");
+//		cliente.setContrasenia("abcd");
+//		cliente.setFechaNacimiento("10/10/1999");
+//
+//		ClienteRepository repository = new ClienteRepository();
+//
+//		repository.guardarCliente(cliente);
+//
+//	}
+//}
+	
 
-		Cliente cliente = new Cliente();
+	    String hash = Encriptador.hash("1234");
 
-		cliente.setNombre("Lucia");
-		cliente.setApellido("Fernandez");
-		cliente.setDni(87654321);
-		cliente.setMail("lucia@gmail.com");
-		cliente.setTelefono("1199988877");
-		cliente.setUsuario("luciaf");
-		cliente.setContrasenia("abcd");
-		cliente.setFechaNacimiento("10/10/1999");
+	    System.out.println(hash);
 
-		ClienteRepository repository = new ClienteRepository();
-
-		repository.guardarCliente(cliente);
-
-	}
-}
+	    System.out.println(Encriptador.verificar("1234", hash));
+	}	}
